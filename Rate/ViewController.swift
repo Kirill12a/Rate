8aa9b8eef743e4e2c43df941da66c6acbe681b9a
Kirill12a <kirilldrozd12@gmail.com>
2022-01-02 23:05:40 +0300

    тестовый запуск

--- Rate/ViewController.swift
+++ Rate/ViewController.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .brown
+        
+        var labelName: UILabel = {
+           var label = UILabel()
+            label.text = "Hello Kirill"
+            label.textColor = .blue
+    
+            return label
+        }()
+        
+        view.addSubview(labelName)
+        labelName.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
         
     }
 
