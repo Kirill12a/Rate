e269cc1f3bfeea06f5d87b273b4eab87dbe75ce7
Kirill12a <kirilldrozd12@gmail.com>
2022-01-02 21:56:41 +0300

    Initial Commit

---
+++ Rate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rate
+//
+//  Created by Kirill Drozdov on 02.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
