4022bab13d7de11563d0f253e4181617d3d46193
Kirill <kirilldrozd12@gmail.com>
2022-01-16 02:29:48 +0300

    Добавил основной функционал, нет норм второго экрана

---
+++ Rate/ImageContinent.swift
@@ -0,0 +1,21 @@
+//
+//  ImageContinent.swift
+//  Rate
+//
+//  Created by Kirill Drozdov on 16.01.2022.
+//
+
+import Foundation
+import UIKit
+
+
+struct DataSource
+{
+  static let icons: [String:String] = [
+    "FSD":"Africa",
+    "fds":"fsda"
+  ]}
+
+
+
+
