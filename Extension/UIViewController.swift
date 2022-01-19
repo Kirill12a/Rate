6fe4ae019cae58f58b61d53f5f52c325be3fa00a
Kirill <kirilldrozd12@gmail.com>
2022-01-19 13:30:49 +0300

    V1 Done

--- Extension/UIViewController.swift
+++ Extension/UIViewController.swift
@@ -9,14 +9,17 @@ import Foundation
 import UIKit
 
 
-extension UIViewController { // убирает клаву при нажатие на экран
-    func setupHideKeyboardOnTap() {
+extension UIViewController
+{
+    func setupHideKeyboardOnTap()
+  {
         self.view.addGestureRecognizer(self.endEditingRecognizer())
         self.navigationController?.navigationBar.addGestureRecognizer(self.endEditingRecognizer())
     }
 
 
-    private func endEditingRecognizer() -> UIGestureRecognizer {
+    private func endEditingRecognizer() -> UIGestureRecognizer
+  {
         let tap = UITapGestureRecognizer(target: self.view, action: #selector(self.view.endEditing(_:)))
         tap.cancelsTouchesInView = false
         return tap
