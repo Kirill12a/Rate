48c47707d4494480b5420543073f243abe7f7895
Kirill <kirilldrozd12@gmail.com>
2022-01-19 13:15:33 +0300

    Обновил проект полностью

---
+++ Extension/UIViewController.swift
@@ -0,0 +1,24 @@
+//
+//  UIViewController.swift
+//  Rate
+//
+//  Created by Kirill Drozdov on 19.01.2022.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIViewController { // убирает клаву при нажатие на экран
+    func setupHideKeyboardOnTap() {
+        self.view.addGestureRecognizer(self.endEditingRecognizer())
+        self.navigationController?.navigationBar.addGestureRecognizer(self.endEditingRecognizer())
+    }
+
+
+    private func endEditingRecognizer() -> UIGestureRecognizer {
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(self.view.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        return tap
+    }
+}
---
+++ Rate/Helpers/ProhibitionOfDifferentViewPositions/AppUtility.swift
@@ -0,0 +1,30 @@
+//
+//  AppUtility.swift
+//  Rate
+//
+//  Created by Kirill Drozdov on 19.01.2022.
+//
+
+import Foundation
+import UIKit
+
+//MARK: - Отменяет поворот экрана
+struct AppUtility {
+
+    static func lockOrientation(_ orientation: UIInterfaceOrientationMask) {
+
+        if let delegate = UIApplication.shared.delegate as? AppDelegate {
+            delegate.orientationLock = orientation
+        }
+    }
+
+    /// OPTIONAL Added method to adjust lock and rotate to the desired orientation
+    static func lockOrientation(_ orientation: UIInterfaceOrientationMask, andRotateTo rotateOrientation:UIInterfaceOrientation) {
+
+        self.lockOrientation(orientation)
+
+        UIDevice.current.setValue(rotateOrientation.rawValue, forKey: "orientation")
+        UINavigationController.attemptRotationToDeviceOrientation()
+    }
+
+}
